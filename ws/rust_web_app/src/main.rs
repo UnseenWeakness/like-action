772c6616a8b1122a389f031c4b56dcc661942676
dusk <en2213a@gmail.com>
2024-09-18 21:55:14 +0800

    fix: 修改端口

--- ws/rust_web_app/src/main.rs
+++ ws/rust_web_app/src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
             // 注册路由
             .service(index)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8081")?
     .run()
     .await
 }
